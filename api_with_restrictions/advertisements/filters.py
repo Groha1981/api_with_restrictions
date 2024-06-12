99c35211bca5bc9c88123abd5987f4cabc1f63f8
Groha <grroohha1981@yandex.ru>
2024-06-12 22:39:43 +0400

    first commit

---
+++ api_with_restrictions/advertisements/filters.py
@@ -0,0 +1,15 @@
+from django_filters import rest_framework as filters, DateFromToRangeFilter, CharFilter
+
+from advertisements.models import Advertisement
+
+
+class AdvertisementFilter(filters.FilterSet):
+    """Фильтры для объявлений."""
+    # TODO: задайте требуемые фильтры
+    created_at = DateFromToRangeFilter()
+    status = CharFilter()
+
+
+    class Meta:
+        model = Advertisement
+        fields = ['status', 'created_at', 'creator']
